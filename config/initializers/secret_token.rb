d2d901cd4ed6825e67dcc0a3a2d1c4edb709b4ff
Grant Brooks <grantbrooks75@gmail.com>
2014-07-30 17:57:15 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zampr::Application.config.secret_key_base = '3591febca41cd778579451a9c53eb539745df8ae437867e1df9fe8e7eef647706cdb6aa1714f13512d706de0c9865927edd5eed64af18fedd0e9a24a3778a25a'
